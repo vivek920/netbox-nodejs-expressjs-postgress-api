91732af04a6a8fc99ceb018bb83c898445d14973
vivek <vivek.thatha@gmail.com>
2024-03-06 16:54:12 +0530

    environmental variables setup

--- connection/connection.js
+++ connection/connection.js
@@ -1,17 +1,45 @@
-const { Sequelize} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
-const sequelize = new Sequelize('netboxdb', 'postgres', 'mysecretpassword', {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
   });
-  
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    });
 
-sequelize.sync({ force: true });
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+User.beforeCreate(async (user) => {
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+});
+
+// Synchronize the database, creating the table if it doesn't exist
+sequelize.sync({ force: true })
+  .then(() => {
+    console.log('Database synchronized.');
+  })
+  .catch((err) => {
+    console.error('Error synchronizing the database:', err);
+  });
+
+module.exports = User;
